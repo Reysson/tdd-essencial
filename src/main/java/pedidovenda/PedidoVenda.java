d540a446ceb3db0f9f021a3af40bffed46657d08
Reysson Martins <reysson.martins30@gmail.com>
2022-07-05 15:27:20 -0400

    test: add more test

--- src/main/java/pedidovenda/PedidoVenda.java
+++ src/main/java/pedidovenda/PedidoVenda.java
@@ -1,19 +1,34 @@
 package pedidovenda;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.DoubleStream;
+
 public class PedidoVenda {
 
     private double valorTotal = 0;
     private double desconto = 0;
+    private List<ItemPedido> itemPedidos = new ArrayList<>();
 
     public void adicionarItem(ItemPedido itemPedido) {
-        valorTotal = itemPedido.getValorUnitario() * itemPedido.getQuantidade();
+        itemPedidos.add(itemPedido);
     }
 
-    public double getValorTotal() {
-        return valorTotal;
+    public ResumoPedido resumoPedido() {
+        double valorTotal = itemPedidos.stream().mapToDouble(i -> i.getQuantidade() * i.getValorUnitario()).sum();
+        double desconto = 0;
+
+        if (valorTotal > 300 && valorTotal <= 800) {
+            desconto = valorTotal * 0.04;
+
+        } else if (valorTotal > 800.0 && valorTotal <= 1000.0) {
+            desconto = valorTotal * 0.06;
+
+        } else if (valorTotal > 1000.0) {
+            desconto = valorTotal * 0.08;
+        }
+
+        return new ResumoPedido(valorTotal, desconto);
     }
 
-    public double getDesconto() {
-        return desconto;
-    }
 }
--- src/test/java/pedidovendatest/PedidoVendaTest.java
+++ src/test/java/pedidovendatest/PedidoVendaTest.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 import pedidovenda.ItemPedido;
 import pedidovenda.PedidoVenda;
+import pedidovenda.ResumoPedido;
 
 import static org.junit.Assert.assertEquals;
 
@@ -15,12 +16,14 @@ public class PedidoVendaTest {
     private PedidoVenda pedidoVenda;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         pedidoVenda = new PedidoVenda();
     }
+
     public void assertResumoPedido(double valorTotal, double desconto) {
-        assertEquals(valorTotal, pedidoVenda.getValorTotal(), 0.0001);
-        assertEquals(desconto, pedidoVenda.getDesconto(), 0.0001);
+        ResumoPedido resumoPedido = pedidoVenda.resumoPedido();
+        assertEquals(valorTotal, resumoPedido.getValorTotal(), 0.0001);
+        assertEquals(desconto, resumoPedido.getDesconto(), 0.0001);
     }
 
     @Test
@@ -38,6 +41,39 @@ public class PedidoVendaTest {
     public void deveCalcularResumoParaUmItemSemDesconto() {
         pedidoVenda.adicionarItem(new ItemPedido("Sabonete", 5.0, 5));
         assertResumoPedido(25.0, 0.0);
+    }
+
+    @Test
+    public void deveCalcularResumoParaDoisItensPedido() {
+        pedidoVenda.adicionarItem(new ItemPedido("Sabonete", 3.0, 3));
+        pedidoVenda.adicionarItem(new ItemPedido("Pasta dental", 7.0, 3));
+
+        assertResumoPedido(30.0, 0);
+    }
+
+    @Test
+    public void deveCalcularDescontoNa1Faixa() {
+        pedidoVenda.adicionarItem(new ItemPedido("Creme", 20.0, 20));
+
+        assertResumoPedido(400.0, 16.0);
+    }
+
+    @Test
+    public void deveAplicarDescontoNa2Faixa() {
+        pedidoVenda.adicionarItem(new ItemPedido("Shampoo", 15.0, 30));
+        pedidoVenda.adicionarItem(new ItemPedido("Óleo", 15.0, 30));
+
+        assertResumoPedido(900.0, 54.0);
+
+    }
+
+    @Test
+    public void deveAplicarDescontoNa3Faixa() {
+        pedidoVenda.adicionarItem(new ItemPedido("Shampoo", 15.0, 30));
+        pedidoVenda.adicionarItem(new ItemPedido("Óleo", 15.0, 30));
+        pedidoVenda.adicionarItem(new ItemPedido("Creme", 20.0, 20));
+
+        assertResumoPedido(1300.0, 104.0);
     }
 
 }
